eb0e6b8a07d3a93251a8300bb6749f60eb8fd088
limaluan <limaluan.dev@gmail.com>
2022-12-20 14:51:23 -0300

    refactor:Header Component now uses TasksContext...
     - refactor: now the Header has a form for newTaskContainer
     - adds: handleAddTask function to create a new Task

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,36 @@ import styles from "./Header.module.css";
 
 import logoImg from "../../assets/logo.svg";
 import plus from "../../assets/plus.svg";
+import { FormEvent, useContext, useState } from "react";
+import { TaskContext } from "../../contexts/TasksContext";
 
 export function Header() {
+  const [description, setDescription] = useState("");
+  const { addTask } = useContext(TaskContext);
+
+  function handleAddTask(e: FormEvent) {
+    e.preventDefault();
+    addTask(description);
+    setDescription("");
+  }
+
   return (
     <header className={styles.headerContainer}>
       <img src={logoImg} alt="" />
-      <div className={styles.newTaskContainer}>
-        <input className={styles.input} placeholder="Adicione uma nova tarefa" />
-        <button className={styles.button}>Criar <img src={plus} /></button>
-      </div>
+      <form
+        className={styles.newTaskContainer}
+        onSubmit={(e) => handleAddTask(e)}
+      >
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className={styles.input}
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button
+          className={styles.button}>Criar <img src={plus} />
+        </button>
+      </form>
     </header>
   )
 }
